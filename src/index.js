8f9b40660384367175e37c7f654f6749db54c563
Alejandro Sanchez <aalejo@gmail.com>
2018-03-23 14:03:40 +0000

    template angine

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 import './style/index.scss';
-import TemplateManager from './js/tm.js';
-
-window.onload = function(){
-    
-    let pieces = document.querySelectorAll('.include-template');
-    let newPieces = [];
-    pieces.forEach(function(elm){
-       newPieces.push({ element: elm, filePath: elm.getAttribute('filepath')});
-    });
-    
-    TemplateManager.loadPieces('',newPieces);
-}
+import TemplateManager from 'html-template-engine';
+window.onload = TemplateManager.start;
