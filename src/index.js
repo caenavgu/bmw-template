cf4a95da2e94952602d4daeecb352019a207e563
Leonardo Aleman <lnrd256@gmail.com>
2018-04-01 21:10:01 -0400

    added gallery

--- src/index.js
+++ src/index.js
@@ -7,14 +7,22 @@ window.onload = function(){
     $(document).ready(function(){
     	let tabselected=null;
 		$('#myTab a').hover( function (e) {
+
 		  $(this).tab('show');
 
 		})
 		 $( ".men" ).mouseleave(function(){
 		 	$(".menu-content").removeClass('active show');
 		 	$('#myTab a').removeClass('active show');
+		 	console.log(1)
 		 });
 	})
+
+// Gets the video src from the data-src on each button
+
+
+
+
 
 
 }
